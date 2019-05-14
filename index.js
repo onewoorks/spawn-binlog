8ed95b12de72398ef8583e12d0aca28699680ec8
onewoorks <irwanbibrahim@onewoorks.com>
2019-05-14 10:45:21 +0800

    add new house

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('-----------------------------------------------------------')
 console.log('Ghost monitoring has been started!!!')
 
 var sample_feeder = require('./houses/sample_feeder')
+var cdccms = require('./houses/cdccms')
 
 console.log('-----------------------------------------------------------')
 
